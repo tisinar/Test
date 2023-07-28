ac0a4eaba900086518fef9bd23d70841465e427a
Jaycrypt <dtech010@gmail.com>
2023-07-28 17:57:37 +0000

    Sklearn repo

---
+++ scikit-learn.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+iris=r['iris']
+df=pd.DataFrame(iris)
+X_iris= df.drop('Species',axis=1)
+Y_iris=df['Species']
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score,confusion_matrix,mean_squared_error
+
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, Y_iris,random_state=1)
+
+model = GaussianNB() # 2. instantiate model
+model.fit(Xtrain,ytrain) # 3. fit model to data
+y_model = model.predict(Xtest)
+accuracy_score(ytest,y_model)
+confusion_matrix(ytest,y_model)
+mean_squared_error(ytest,y_model)
+
+
+import tensorflow as tf
+from tensorflow import keras
+import torch as pt
+
+
+
+
