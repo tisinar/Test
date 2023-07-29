82389806677494884bb21637ebc7749bf024416a
Jaycrypt <84263401+dtech010@users.noreply.github.com>
2023-07-29 01:35:51 +0000

    numpy doc

---
+++ numpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+a=np.array([(1.5,2,3), (4,5,6)], dtype = float)
+b=np.array([[(1.5,2,3), (4,5,6)],[(3,2,1), (4,5,6)]], dtype = float)
